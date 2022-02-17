ba83d6e543de15eccff505d13fbf791dc7c4e5e3
Eren Çifci <eren.cifci04@gmail.com>
2022-02-18 00:15:59 +0300

    Add relation ship emoji role

--- events/client/ready.js
+++ events/client/ready.js
@@ -21,22 +21,32 @@ module.exports = async (client) => {
     //         await channel.messages.fetch("943947304661049394").then(msg => msg.edit(''));
     //     });
 
-    //     let emoji = (id) => client.emojis.cache.find((emoji) => emoji.id === id);
+    let emoji = (id) => client.emojis.cache.find((emoji) => emoji.id === id);
 
-    //     client.guilds.cache
-    //         .first()
-    //         .channels.fetch()
-    //         .then(async (channels) => {
-    //             let channel = channels.find(
-    //                 (channel) => channel.id === "940666639953846314"
-    //             );
-    //             const message = await channel.send(`***__Bildirim Rolleri__***
+    client.guilds.cache
+        .first()
+        .channels.fetch()
+        .then(async (channels) => {
+            let channel = channels.find(
+                (channel) => channel.id === "940666640775938101"
+            );
+            const message = await channel.send(`***__İlişki Rolleri__***
 
-    // **Çekiliş Bildirimi: ${emoji(emojis.cekilis)}**
+    **Evli: ${emoji(emojis.evli)}**
+    
+    **Sevgilim Var: ${emoji(emojis.sevgilimvar)}**
+    
+    **Sevgilim Yok: ${emoji(emojis.sevgilimyok)}**
+    
+    **Sevdiğim Var: ${emoji(emojis.sevdigimvar)}**
+    
+    **Sevgili Yapmıyorum: ${emoji(emojis.sevgiliyapmiyorum)}**
+    `);
 
-    // **Etkinlik Bildirimi: ${emoji(emojis.etkinlik)}**`);
-
-    //             await message.react(emoji(emojis.cekilis));
-    //             await message.react(emoji(emojis.etkinlik));
-    //         });
+            await message.react(emoji(emojis.evli));
+            await message.react(emoji(emojis.sevgilimvar));
+            await message.react(emoji(emojis.sevgilimyok));
+            await message.react(emoji(emojis.sevdigimvar));
+            await message.react(emoji(emojis.sevgiliyapmiyorum));
+        });
 };
