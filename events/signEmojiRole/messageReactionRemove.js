561072ee9fe6fc209e5074271da19e6a48dbec32
Eren Çifci <eren.cifci04@gmail.com>
2022-02-17 17:08:36 +0300

    Add signEmojiRole

---
+++ events/signEmojiRole/messageReactionRemove.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+const { check } = require("../../functions/role");
+
+/**
+ * @param {Discord.Client} client
+ * @param {Discord.MessageReaction} reaction
+ */
+
+module.exports = async (client, reaction, user) => {
+    check(reaction, user, "signRole").then(async (role) => {
+        const guild = reaction.message.guild;
+        const member = await guild.members
+            .fetch()
+            .then((members) => members.find((member) => member.id === user.id));
+
+        if (!role) return;
+
+        if (!member.roles.cache.has(role.id)) return;
+
+        await member.roles.remove(role, "Rol Sistemi").catch(console.error);
+    });
+};
